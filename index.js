51d4ec849929ebef7e639e8731690717e654efe3
hasib <hamidhasibul@gmail.com>
2023-10-22 03:31:08 +0600

    Star tech data scrap tested

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 import puppeteer from "puppeteer";
 
+import { promises as fs } from "fs";
+
 const launchScrapper = async () => {
   let browser;
   try {
     browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://www.startech.com.bd/component/graphics-card");
 
-    page.getDefaultTimeout(2 * 60 * 1000);
+    /* const selector = ".main-content";
+    await page.waitForSelector(selector);
 
-    //   await page.screenshot({ path: "screenshot.png" });
+    const el = await page.$(selector);
 
-    const html = await page.evaluate(() => {
-      return document.documentElement.outerHTML;
+    const text = await el.evaluate((e) => e.innerHTML);
+
+    console.log(text); */
+
+    const selector = ".p-item";
+    await page.waitForSelector(selector);
+
+    const productData = await page.$$eval(".p-item", (items) => {
+      return items.map((item) => {
+        const name = item.querySelector(".p-item-name a").textContent;
+        const description =
+          item.querySelector(".short-description").textContent;
+        const price = item.querySelector(".p-item-price span").textContent;
+
+        const trimmedDesc = description.trim().split("\n").join(", ");
+        return { name, trimmedDesc, price };
+      });
     });
 
-    console.log(html);
+    console.log(productData);
+
+    // Save Data to JSON
+
+    fs.writeFile(
+      "product-details.json",
+      JSON.stringify(productData),
+      (error) => {
+        if (error) throw new Error(error);
+
+        console.log("File Saved");
+      }
+    );
   } catch (error) {
     console.error(`Scrapping Failed`, error);
   } finally {
